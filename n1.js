b6db96b5f374a33eee418692e7321c0353188f7a
Crazy4Redstone <mcpe1398@gmail.com>
2014-03-19 12:43:51 -0500

    Update n1.js

--- n1.js
+++ n1.js
@@ -9,15 +9,19 @@ ModPE.setItem (372,'nether_wart',0,'Nether Wart Seeds')
 
 Block.defineBlock (88,'Soul Sand',[['soul_sand',0]],2,false,7)
 Block.setShape (88,0,0,0,1,15/16,1)
+Block.setDestroyTime (88,5)
 
-Block.defineBlock (115,'Nether Wart S1',[['nether_wart_stage_x',0]],50,false,1)
+Block.defineBlock (115,'Nether Wart S1',[['nether_wart_stage_x',0]],50,false,6)
 Block.setRenderLayer (115,1)
+Block.setDestroyTime (115,1)
 
-Block.defineBlock (116,'Nether Wart S2',[['nether_wart_stage_x',1]],50,false,1)
+Block.defineBlock (116,'Nether Wart S2',[['nether_wart_stage_x',1]],50,false,6)
 Block.setRenderLayer (116,1)
+Block.setDestroyTime (116,1)
 
-Block.defineBlock (117,'Nether Wart S3',[['nether_wart_stage_x',2]],50,false,1)
+Block.defineBlock (117,'Nether Wart S3',[['nether_wart_stage_x',2]],50,false,6)
 Block.setRenderLayer (117,1)
+Block.setDestroyTime (117,1)
 
 // Code
 
