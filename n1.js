d6de16a53f01f6b3022977ff1441aeddb129940e
Crazy4Redstone <mcpe1398@gmail.com>
2014-03-19 12:37:03 -0500

    Update n1.js

--- n1.js
+++ n1.js
@@ -10,11 +10,11 @@ ModPE.setItem (372,'nether_wart',0,'Nether Wart Seeds')
 Block.defineBlock (88,'Soul Sand',[['soul_sand',0]],2,false,7)
 Block.setShape (88,0,0,0,1,15/16,1)
 
-Block.defineBlock (115,'Nether Wart S1',[['nether_wart_stage_x',0]],50,true,6)
+Block.defineBlock (115,'Nether Wart S1',[['nether_wart_stage_x',0]],50,false,1)
 
-Block.defineBlock (116,'Nether Wart S2',[['nether_wart_stage_x',1]],50,true,6)
+Block.defineBlock (116,'Nether Wart S2',[['nether_wart_stage_x',1]],50,false,1)
 
-Block.defineBlock (117,'Nether Wart S3',[['nether_wart_stage_x',2]],50,true,6)
+Block.defineBlock (117,'Nether Wart S3',[['nether_wart_stage_x',2]],50,false,1)
 
 // Code
 
@@ -23,6 +23,14 @@ function useItem (x,y,z,itemId,blockId,side)
   if (itemId == 372 && blockId == 88 && side == 1)
   {
     setTile (x,y+1,z,115)
+  }
+  else if (itemId == 351:15 && blockId == 115)
+  {
+    setTile (x,y,z,116)
+  }
+  else if (itemId == 351:15 && blockId == 116)
+  {
+    setTile (x,y,z,117)
   }
 }  
 
