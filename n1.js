bef73336a616e45c6503ff8b34004f3a3b1b3146
Crazy4Redstone <mcpe1398@gmail.com>
2014-03-19 12:47:06 -0500

    Update n1.js

--- n1.js
+++ n1.js
@@ -9,19 +9,19 @@ ModPE.setItem (372,'nether_wart',0,'Nether Wart Seeds')
 
 Block.defineBlock (88,'Soul Sand',[['soul_sand',0]],2,false,7)
 Block.setShape (88,0,0,0,1,15/16,1)
-Block.setDestroyTime (88,5)
+Block.setDestroyTime (88,1.5)
 
 Block.defineBlock (115,'Nether Wart S1',[['nether_wart_stage_x',0]],50,false,6)
 Block.setRenderLayer (115,1)
-Block.setDestroyTime (115,1)
+Block.setDestroyTime (115,0.50)
 
 Block.defineBlock (116,'Nether Wart S2',[['nether_wart_stage_x',1]],50,false,6)
 Block.setRenderLayer (116,1)
-Block.setDestroyTime (116,1)
+Block.setDestroyTime (116,0.50)
 
 Block.defineBlock (117,'Nether Wart S3',[['nether_wart_stage_x',2]],50,false,6)
 Block.setRenderLayer (117,1)
-Block.setDestroyTime (117,1)
+Block.setDestroyTime (117,0.50)
 
 // Code
 
@@ -32,15 +32,13 @@ function useItem (x,y,z,itemId,blockId,side)
     setTile (x,y+1,z,115)
     addItemInventory (372,-1)
   }
-  else if (itemId == 351,15 && blockId == 115)
+  else if (itemId == 0 && blockId == 115)
   {
     setTile (x,y,z,116)
-    addItemInventory (351,15,-1)
   }
-  else if (itemId == 351,15 && blockId == 116)
+  else if (itemId == 0 && blockId == 116)
   {
     setTile (x,y,z,117)
-    addItemInventory (351,15,-1)
   }
 }  
 
